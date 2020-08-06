2218178844222d425f34d0885737a5d762f42245
Rajat Gundi <glrajat@gmail.com>
2020-08-06 16:48:34 +0530

    Calculating Daily Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,3 +11,23 @@ then
 else
 	echo "Employee is absent"
 fi
+
+#Calculating daily Employee Wage
+partTime=8
+fullTime=12
+wagePerHour=20
+randomWorkTime=$((RANDOM%2))
+if [[ $empCheck -eq 1 ]]
+then
+	case $randomWorkTime in 
+				1) echo "Employee is working full time"
+				   echo "Employee wage for the day : Rs $(( $fullTime * $wagePerHour ))"
+				   ;;
+				*) echo "Employee is working part time"
+				   echo "Employee wage for the day : Rs $(( $partTime * $wagePerHour ))"
+				   ;;
+	esac
+else
+	echo "Employee wage for the day : Rs 0"
+fi
+
