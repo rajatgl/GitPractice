c6bb1642bc6816e88e69970aaeaa0df817e98189
Rajat Gundi <glrajat@gmail.com>
2020-08-07 22:08:17 +0530

    Functional update & Calculating MonthlyWage (20 days/month assumption)

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,29 +5,57 @@ echo "Welcome To Main Branch"
 
 #Checking whether employee is present or absent
 empCheck=$((RANDOM%2))
-if [[ $empCheck -eq 1 ]]
-then
-	echo "Employee is present"
-else
-	echo "Employee is absent"
-fi
+function getEmployeeAttendance()
+{
+	if [[ $1 -eq 1 ]]
+	then
+		echo "Employee is present"
+	else
+		echo "Employee is absent"
+	fi
+}
+getEmployeeAttendance $empCheck
+
 
 #Calculating daily Employee Wage
 partTime=8
 fullTime=12
 wagePerHour=20
 randomWorkTime=$((RANDOM%2))
-if [[ $empCheck -eq 1 ]]
-then
-	case $randomWorkTime in 
-				1) echo "Employee is working full time"
-				   echo "Employee wage for the day : Rs $(( $fullTime * $wagePerHour ))"
+function getDailyWage()
+{
+	if [[ $1 -eq 1 ]]
+	then
+		case $2 in 
+				1) #echo "Employee is working full time"
+				   #echo "Employee wage for the day : Rs $(( $fullTime * $wagePerHour ))"
+				   wageForDaDay=$(( $fullTime * $wagePerHour ))
 				   ;;
-				*) echo "Employee is working part time"
-				   echo "Employee wage for the day : Rs $(( $partTime * $wagePerHour ))"
+				*) #echo "Employee is working part time"
+				   #echo "Employee wage for the day : Rs $(( $partTime * $wagePerHour ))"
+				   wageForDaDay=$(( $partTime * $wagePerHour ))
 				   ;;
-	esac
-else
-	echo "Employee wage for the day : Rs 0"
-fi
+		esac
+	else
+		#echo "Employee wage for the day : Rs 0"
+		wageForDaDay=0
+	fi
+}
+getDailyWage $empCheck $randomWorkTime
+echo "Day's wage for employee is : Rs$wageForDaDay"
 
+#Calculating Montly Wage ( assuming 20 days per month )
+function getMonthlyWage()
+{
+	MonthlyWage=0
+	for (( i=1 ; i<=20 ; i++ ))
+	do
+		getDailyWage $empCheck $randomWorkTime
+		wagePerDay=$wageForDaDay
+		empCheck=$((RANDOM%2))
+		randomWorkTime=$((RANDOM%2))
+		MonthlyWage=$(($MonthlyWage + $wagePerDay))
+	done
+	echo "MOnthly Wage of the employee : Rs$MonthlyWage"
+}
+getMonthlyWage
